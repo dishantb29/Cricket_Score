85050e81e8aea616ae80a4e7ee16c3bfb716e32d
ForNotWork <120727683+ForNotWork@users.noreply.github.com>
2022-12-27 09:34:58 +0530

    Update Cricket_Game.c

--- Cricket_Game.c
+++ Cricket_Game.c
@@ -8,15 +8,26 @@ int win()
     {
         return 1;
     }
-    else if(Remaining_Players == 10)
+    else if(Remaining_Players == 0)
     {
         return 0;
     }
     else
         return -1;
 }
-int Score_Board()
+int AI_Score_Board(int RunI,int WicketI)
 {
+    int Score_Run,Score_Wicket;
+    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
+    printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
+    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
+    printf("\n");
+    printf("RUN:%d\t\t",Score_Run);
+    printf("WICKET:%d\t\n",Score_Wicket);
+}
+int Player_Score_Board(int RunP,int WicketP)
+{
+    int Score_Run,Score_Wicket,Remaining_Runs;
     printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
     printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
     printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
@@ -44,29 +55,16 @@ int Wicket()
 
 int main()
 {
+    int AI_Wicket[10]={1,5,3,7,4};
+    int AI_SCORE[10]={24,20,15,6,8,12,6,11,9};
     time_t t;
     int Runs[10]={0,1,2,4,6},AI_SCORE,PLAYER_SCORE;
     char out[20]={"RunOut","BOLD","Catch"};
-    int R,O,hit;
+    int RI,OI,hit;
     srand((unsigned)time(&t));
-    printf("Player turn:");
-    hit = rand()%3;
-    switch (hit)
-    {
-    case 1:
-        while (AI_SCORE>PLAYER_SCORE)
-        {
-            printf("");
-            R=rand()%6;
-            int Total_Score =Runs[R];
-        }
-        
-        break;
-    case 2:
-
-        break;
-    default:
-        break;
-    }
+    RI = rand()%10;
+    OI = rand()%6;
+    Score_Board(AI_SCORE[RI],AI_Wicket[OI]);
+    
     return 0;
 }
